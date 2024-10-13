ae1d45081488256fff2a694874d9b503b36ac6b2
mohamedghaly140 <mohamedghaly140@gmail.com>
2024-10-14 02:38:15 +0300

    added simple ui and text breif for the extention

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import "./App.css";
 
 const AutoCompletePopup: React.FC = () => {
   const enableAutocomplete = () => {
@@ -12,9 +13,28 @@ const AutoCompletePopup: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <h1>App</h1>
-      Auto Complete Extention
+    <div className="home-container">
+      <div className="content">
+        <h1>Auto-Complete Extension</h1>
+        <p>
+          Welcome to the Auto-Complete Extension! This extension helps you
+          automatically fill in your credentials on websites that disable
+          browser autofill features. Just enable the extension, and it will take
+          care of the rest!
+        </p>
+        <p>
+          With our extension, you can easily manage your username and password
+          autofill settings, allowing for a smoother and more efficient browsing
+          experience.
+        </p>
+        <div className="icon-container">
+          <img
+            src="icon_logo_128px.png"
+            alt="Auto-Complete Icon"
+            className="icon"
+          />
+        </div>
+      </div>
     </div>
   );
 };
